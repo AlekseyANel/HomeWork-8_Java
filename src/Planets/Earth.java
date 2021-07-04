f96b8652b6e88c231516f7cbf5f87f97150fe948
Neliubin Oleksiy <aa7n2012@gmail.com>
2021-07-04 20:16:52 +0300

    HW#8 second task коррект

--- src/Planets/Earth.java
+++ src/Planets/Earth.java
@@ -1,9 +1,15 @@
 package Planets;
 
-public class Earth extends MainPlanets.Planet implements MainPlanets.PlanetInterface {
+public class Earth extends Planet implements PlanetInterface {
+    String plName="Земля";
     int number = 3;
     double mas = 597360000;
     float G = (float) 9.780327;
+
+
+    String getPlName() {
+        return plName;
+    }
 
     int getNumber() {
         return number;
